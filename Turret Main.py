49802593ecb4e62478996932c64dacab216e1c70
HEXA-SOFTWARE-DEV <scottjohnjames0.8@gmail.com>
2023-09-08 18:59:03 +0100

    Fixed Servo Motor Error
    Lines 20, 25, and 33: fixed syntax error: replaced ...angles(0) with: ...angles = 0.
    
    Now works properly on the raspberry Pi!

--- Turret Main.py
+++ Turret Main.py
@@ -1,42 +1,57 @@
 
 # Camera Imports
 import cv2
-import PiCamera
+#import Picamera
 
 # Servo Imports
 from adafruit_servokit import ServoKit as Servo
-kit = Servo(channels=16) # The Adafruit Raspberry Pi hat for the Servos has 16 Pwm Channels to use.
-                         # Here I am making all of the channels active
+
 
 # Maths and Time Imports
 import numpy as np
 import os
 import time
-from matplotlib import pyplot as pltd
+#]from matplotlib import pyplot as pltd
+
+kit = Servo(channels = 16) # The Adafruit Raspberry Pi hat for the Servos has 16 Pwm Channels to use.
+                           # Here I am making all of the channels active
+
 
 def InitialiseAndCalibrate (kit):
-
+    
+    print (type(kit))
+    
     # Enable Servos and Set to 0
-    kit.servo[0].angle(x) # In this case x denotes any angle variable for the time being, probably 0 degrees (max 180 degrees)
+    kit.servo[0].angle = 0 # In this case x denotes any angle variable for the time being, probably 0 degrees (max 180 degrees)
                           # servo[0] denotes the PWM channel (Which Motor is turning)
     time.sleep(3) # Wait 3 seconds
 
-    kit.servo[0].angle(y) # In this case y denotes any angle variable for the time being, probably 180 degrees
+    kit.servo[0].angle = 180 # In this case y denotes any angle variable for the time being, probably 180 degrees
 
     time.sleep(3) # Wait 3 seconds 
 
-    kit.servo[0].angle(x) # Sets angle back to original position
+    kit.servo[0].angle = 0 # Sets angle back to original position
 
     # Enable camera
-    cap = cv2.VideoCapture(0) #cv2 will use Camera 0 (Default camera), in this case is the Raspberry Pi camera
-    cap.set(3, 640) # Setting the Height,
-    cap.set(4, 420) # and Width of the capture
+    while (True):
+        
+        cap = cv2.VideoCapture(0) #cv2 will use Camera 0 (Default camera), in this case is the Raspberry Pi camera
+        
+        #Set Capture Frame
+        frame = cap.read
+        
+        #Display the frame
+        cv2.imshow('frame', frame)
+        
+        cap.set(3, 640) # Setting the Height,
+        cap.set(4, 420) # and Width of the capture
 
     return cap
 
 def LookforPeople(cap):
-    Personfound = False
-    while Personfound == False:
-        #Keep Looking
     
     return Personfound # Filler Variable to signify that a person is found on the feed
+
+###################Main############################
+    
+cap = InitialiseAndCalibrate (kit)
