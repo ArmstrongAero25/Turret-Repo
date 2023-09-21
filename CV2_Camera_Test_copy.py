4b2c7b4754c45edd58b45d5568ca766d324d5241
Armstrong Aero <100852623+HEXA-SOFTWARE-DEV@users.noreply.github.com>
2023-09-21 10:20:00 +0100

    Changed Commenting

--- CV2_Camera_Test_copy.py
+++ CV2_Camera_Test_copy.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-cv2.startWindowThread()
-cap = cv2.VideoCapture(-1, cv2.CAP_V4L2)
+cv2.startWindowThread() # Start the Window
+cap = cv2.VideoCapture(-1, cv2.CAP_V4L2) # Start the Capture
 # Check if the webcam is opened correctly
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
 while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
-    gray = cv2.cvtColor(np.float32(frame), cv2.COLOR_BGR2GRAY)
+    ret, frame = cap.read() # Read the input video
+    frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA) # Resize the frame
+    cv2.imshow('Input', frame) # Display the Frame
+    gray = cv2.cvtColor(np.float32(frame), cv2.COLOR_BGR2GRAY) # Make the Frame gray
     
     # read haacascade to detect faces in input image
     Person_cascade = cv2.CascadeClassifier("Desktop/haarcascade_fullbody.xml")
@@ -19,7 +19,7 @@ while True:
     # detects people in the input image
     gray = np.array(gray, dtype = 'uint8')
 
-    People = Person_cascade.detectMultiScale(gray, 1.1, 2, 0)
+    People = Person_cascade.detectMultiScale(gray, 1.1, 2, 0) # Find People in the Image
     print('Number of detected People:', len(People))
 
     # loop over all the detected people
