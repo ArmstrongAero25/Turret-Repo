af1791ab0f3e8663294f991778a2f015948f29d3
HEXA-SOFTWARE-DEV <scottjohnjames0.8@gmail.com>
2023-10-01 17:55:17 +0100

    Might speed up fps?

--- CV2_Camera_Test_copy.py
+++ CV2_Camera_Test_copy.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
+import threading
 
 cv2.startWindowThread() # Start the Window
 cap = cv2.VideoCapture(-1, cv2.CAP_V4L2) # Start the Capture
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+cap.set(cv2.CAP_PROP_FPS, 30)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 # Check if the webcam is opened correctly
 if not cap.isOpened():
