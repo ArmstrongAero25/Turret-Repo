287c23b007ee61c5746ccf50168d96c535bfc133
HEXA-SOFTWARE-DEV <scottjohnjames0.8@gmail.com>
2023-10-03 21:49:01 +0100

    trying to improve performance (again)

--- CV2_Camera_Test_copy.py
+++ CV2_Camera_Test_copy.py
@@ -3,17 +3,16 @@ import numpy as np
 from gfd.py.video.capture import VideoCaptureThreading
 
 cv2.startWindowThread() #make the window
-cap = VideoCaptureThreading(-1, cv2.CAP_V4L2) # Start the Capture
+cap = VideoCaptureThreading(cv2.CAP_V4L2) # Start the Capture
 
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-cap.set(cv2.CAP_PROP_FPS, 28)
+cap.set(cv2.CAP_PROP_FPS, 30)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
 cap.start()
 while True:
 
     ret, frame = cap.read() # Read the input video/stream
-    frame = cv2.resize(frame, None, fx=0.6, fy=0.5, interpolation=cv2.INTER_AREA) # Resize the frame
     gray = cv2.cvtColor(np.float32(frame), cv2.COLOR_BGR2GRAY) # Make the Frame gray
         
     # read haacascade to detect people in input image
@@ -21,7 +20,7 @@ while True:
 
     # detects people in the input image
     gray = np.array(gray, dtype = 'uint8')
-    People = Person_cascade.detectMultiScale(gray, 2.0, 3, 40, 40) # Find People in the Image
+    People = Person_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=4,maxSize=(100,100), minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE) # Find People in the Image
     print('Number of detected People:', len(People))
 
     # loop over all the detected people
@@ -29,12 +28,14 @@ while True:
         # To draw a rectangle around the detected person  
         cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,255),2)
         
-    cv2.imshow('Output', frame) # Display the Frame
-
+    
     # Stop the code when the ESC key is pressed.
     c = cv2.waitKey(1)
     if c == 27:
         break
 
-cap.stop()
+    cap.stop()
+
+cv2.imshow('Output', frame) # Display the Frame
+
 cv2.destroyAllWindows()
