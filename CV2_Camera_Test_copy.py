0222f14719b555f5eb9e141dff983b1592e94872
Armstrong Aero <100852623+HEXA-SOFTWARE-DEV@users.noreply.github.com>
2023-10-03 11:59:04 +0100

    Update CV2_Camera_Test_copy.py
    Testing weather or not a different format to the code will work?
    
    moved the Output to below the drawing of the rectangle

--- CV2_Camera_Test_copy.py
+++ CV2_Camera_Test_copy.py
@@ -6,7 +6,6 @@ cv2.startWindowThread() #make the window
 cap = VideoCaptureThreading(-1, cv2.CAP_V4L2) # Start the Capture
 
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-
 cap.set(cv2.CAP_PROP_FPS, 28)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
@@ -14,10 +13,7 @@ cap.start()
 while True:
 
     ret, frame = cap.read() # Read the input video
-
     frame = cv2.resize(frame, None, fx=0.6, fy=0.5, interpolation=cv2.INTER_AREA) # Resize the frame
-
-    cv2.imshow('Output', frame) # Display the Frame
     gray = cv2.cvtColor(np.float32(frame), cv2.COLOR_BGR2GRAY) # Make the Frame gray
         
     # read haacascade to detect people in input image
@@ -36,6 +32,6 @@ while True:
     c = cv2.waitKey(1)
     if c == 27:
         break
-cap.stop()
-
+    cv2.imshow('Output', frame) # Display the Frame
+    cap.stop()
 cv2.destroyAllWindows()
