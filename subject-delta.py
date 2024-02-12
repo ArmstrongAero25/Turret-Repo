54f10c61bbfa8cdd7b3a9b8a7027e561123ad81a
Armstrong Aero <100852623+HEXA-SOFTWARE-DEV@users.noreply.github.com>
2024-02-12 11:33:04 +0000

    Add files via upload

---
+++ subject-delta.py
@@ -0,0 +1,66 @@
+import numpy as np
+import cv2
+from adafruit_servokit import ServoKit
+
+def Main():
+
+    width, height = 640, 480
+
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    cv2.startWindowThread()
+    cap = cv2.VideoCapture(0)
+
+    kit = ServoKit(channels=16)
+
+    while(True):
+        
+        ret, frame = cap.read()   
+        frame = cv2.resize(frame, (width, height))    
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    
+        boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+        for (xA, yA, xB, yB) in boxes:
+            
+            cv2.rectangle(frame, (xA, yA), (xB, yB),(0, 255, 0), 2)
+            if cv2.rectangle:
+                print("Detected")
+                turn_servo(xA, yA, xB, yB)
+            
+        
+    
+        
+        
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+    cap.release() # Release Capture
+    cv2.destroyAllWindows() # Destroy Window
+    cv2.waitKey(1)
+
+def turn_servo(xA, yA, xB, yB, kit):
+    servo_speed = 10
+    DeltaX = xA - xB // servo_speed
+    DeltaY = yA - yB // servo_speed
+
+    pan_servo_position += DeltaX
+    tilt_servo_position += DeltaY
+
+    pan_servo_position = max(0, min(180, pan_servo_position))
+    tilt_servo_position = max(0, min(180, tilt_servo_position))
+
+    kit.servo[0].angle = pan_servo_position
+    kit.servo[1].angle = tilt_servo_position
+
+
+##### Main ####
+
+if __name__ == "__Main__":
+    Main()
