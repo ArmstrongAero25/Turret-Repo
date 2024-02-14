2d34dafcef6c7c978d02cafa63dcad2376ce854f
HEXA-SOFTWARE-DEV <scottjohnjames0.8@gmail.com>
2024-02-14 20:46:28 +0000

    Update subject-delta.py

--- subject-delta.py
+++ subject-delta.py
@@ -24,32 +24,29 @@ def Main():
             print('--(!) No captured frame -- Break!')
             break
         find_People(frame)
-
-
-	cv2.imshow('frame',frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+        
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+             break
 
 
     cap.release() # Release Capture
     cv2.destroyAllWindows() # Destroy Window
     cv2.waitKey(1)
 
-def find_People(frame):
-	frame = cv2.resize(frame, (width, height))
-	gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+def find_People(frame, kit):
+     width, height = 640, 480
+     frame = cv2.resize(frame, (width, height))
+     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-	boxes, weights = hog.detectMultiScale(gray, winStride=(8,8) )
-	boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-
-	for (xA, yA, xB, yB) in boxes:
-
-	cv2.rectangle(frame, (xA, yA), (xB, yB),(0, 255, 0), 2)
-	if cv2.rectangle:
-		print("Detected")
-		turn_servo(xA, yA, xB, yB, kit)
-
-	return frame
+     boxes, weights = hog.detectMultiScale(gray, winStride=(8,8) )
+     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+     
+     for (xA, yA, xB, yB) in boxes:
+        cv2.rectangle(frame, (xA, yA), (xB, yB),(0, 255, 0), 2)
+        if cv2.rectangle:
+            print("Detected")
+            turn_servo(xA, yA, xB, yB, kit)
 
 
 def turn_servo(xA, yA, xB, yB, kit):
