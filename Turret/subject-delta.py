be238049b015427d998b3ca65c2441d96dbed8a7
HEXA-SOFTWARE-DEV <scottjohnjames0.8@gmail.com>
2024-02-16 17:39:21 +0000

    reorganised files

--- Turret/subject-delta.py
+++ Turret/subject-delta.py
@@ -1,71 +1,71 @@
 import numpy as np
-import time
 import cv2
 from adafruit_servokit import ServoKit
-import vlc
 
 
-def Main():
+def main():
+    print("HELLO WORLD")
     width, height = 640, 480
     kit = ServoKit(channels=16)
     servo_speed = 10
-    
+
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-    
+
     cv2.startWindowThread()
-    cap = cv2.VideoCapture(0)
-    
-    if not cap.isOpened:
-        print('--(!)Error opening video capture')
+    cap = cv2.VideoCapture(0 + cv2.CAP_V4L2)
+
+    if not cap.isOpened():
+
+        print("--(!)Error opening video capture")
         exit(0)
-    
-    PlayBuildSound()
-    
-    while (True):
+
+    while True:
+
         ret, frame = cap.read()
         if not ret:
+            print("BAD")
             break
 
         elif frame is None:
             print('--(!) No captured frame -- Break!')
             break
-            
-        find_People(frame, hog, width, height, kit, servo_speed)
+
+        frame = find_People(frame, hog, width, height, kit, servo_speed)
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
-        cap.release()  # Release Capture
-        cv2.destroyAllWindows()  # Destroy Window
-        cv2.waitKey(1)
+
+    cap.release()  # Release Capture
+    cv2.destroyAllWindows()  # Destroy Window
+    cv2.waitKey(1)
 
 
-def find_People(frame, kit, servo_speed, hog, width, height):   
+def find_People(frame, hog, width, height, kit, servo_speed):
+
     frame = cv2.resize(frame, (width, height))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    
+
     boxes, weights = hog.detectMultiScale(gray, winStride=(8, 8))
     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-    
+
     for (xA, yA, xB, yB) in boxes:
+
         cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
-        if cv2.rectangle:
-            print("Detected")
-            turn_servo(xA, yA, xB, yB, kit, servo_speed)
-            
+        print("Detected")
+        turn_servo(xA, yA, xB, yB, kit, servo_speed)
+
     return frame
 
-def PlayBuildSound():
-    p = vlc.MediaPlayer("BuildinASentry.mp3")
-    p.play()
-    time.sleep(2)
-    p.stop()
-    
+
 def turn_servo(xA, yA, xB, yB, kit, servo_speed):
-    DeltaX = xA - xB // servo_speed
-    DeltaY = yA - yB // servo_speed
+
+    pan_servo_position = 0  # Initialize pan servo position
+    tilt_servo_position = 0  # Initialize tilt servo position
+
+    DeltaX = (xA + xB) // servo_speed
+    DeltaY = (yA + yB) // servo_speed
 
     pan_servo_position += DeltaX
     tilt_servo_position += DeltaY
@@ -77,7 +77,5 @@ def turn_servo(xA, yA, xB, yB, kit, servo_speed):
     kit.servo[1].angle = tilt_servo_position
 
 
-##### Main #####
-
-if __name__ == "__Main__":
-  Main()
+if __name__ == "__main__":
+    main()
