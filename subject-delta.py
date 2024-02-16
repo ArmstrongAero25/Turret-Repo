768aba635355fa328250c77bf0eb2ea830d720a4
HEXA-SOFTWARE-DEV <scottjohnjames0.8@gmail.com>
2024-02-16 15:12:15 +0000

    Update subject-delta.py

--- subject-delta.py
+++ subject-delta.py
@@ -4,6 +4,7 @@ from adafruit_servokit import ServoKit
 
 
 def Main():
+
     width, height = 640, 480
     kit = ServoKit(channels=16)
     servo_speed = 10
@@ -14,11 +15,12 @@ def Main():
     cv2.startWindowThread()
     cap = cv2.VideoCapture(0)
 
-    if not cap.isOpened:
+    if not cap.isOpened():
+
         print('--(!)Error opening video capture')
         exit(0)
 
-    while (True):
+    while True:
 
         ret, frame = cap.read()
         if not ret:
@@ -28,35 +30,42 @@ def Main():
         elif frame is None:
             print('--(!) No captured frame -- Break!')
             break
-        find_People(frame, hog, width, height, kit, servo_speed)
+
+        frame = find_People(frame, hog, width, height, kit, servo_speed)
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        cap.release()  # Release Capture
-        cv2.destroyAllWindows()  # Destroy Window
-        cv2.waitKey(1)
+    cap.release()  # Release Capture
+    cv2.destroyAllWindows()  # Destroy Window
+    cv2.waitKey(1)
 
 
-def find_People(frame, kit, servo_speed, hog, width, height):
+def find_People(frame, hog, width, height, kit, servo_speed):
+
     frame = cv2.resize(frame, (width, height))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
     boxes, weights = hog.detectMultiScale(gray, winStride=(8, 8))
     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
     for (xA, yA, xB, yB) in boxes:
+
         cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
-        if cv2.rectangle:
-            print("Detected")
-            turn_servo(xA, yA, xB, yB, kit, servo_speed)
+        print("Detected")
+        turn_servo(xA, yA, xB, yB, kit, servo_speed)
 
     return frame
 
 
 def turn_servo(xA, yA, xB, yB, kit, servo_speed):
-    DeltaX = xA - xB // servo_speed
-    DeltaY = yA - yB // servo_speed
+
+    pan_servo_position = 90  # Initialize pan servo position
+    tilt_servo_position = 90  # Initialize tilt servo position
+
+    DeltaX = (xA + xB) // servo_speed
+    DeltaY = (yA + yB) // servo_speed
 
     pan_servo_position += DeltaX
     tilt_servo_position += DeltaY
@@ -68,7 +77,5 @@ def turn_servo(xA, yA, xB, yB, kit, servo_speed):
     kit.servo[1].angle = tilt_servo_position
 
 
-##### Main ####
-
-if __name__ == "__Main__":
+if __name__ == "__main__":
     Main()
