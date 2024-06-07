3d0e6c3320866c72d5ecc0679ef3948485ccb2c5
HEXA-SOFTWARE-DEV <scottjohnjames0.8@gmail.com>
2024-06-07 17:23:07 +0100

    back to where we started

--- Turret/Turret_Code.py
+++ Turret/Turret_Code.py
@@ -56,9 +56,7 @@ def main():
 def find_people(frame, hog, width, height, servo_speed):
     # This will find a target within a given frame.
 
-    # frame = cv2.resize(frame, (width, height))
-    # frame = np.array(frame, dtype=np.uint8)
-    frame = np.full((100, 80, 3), 12, dtype=np.uint8)
+    frame = cv2.resize(frame, (width, height))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
     boxes, _ = hog.detectMultiScale(gray, winStride=(8, 8))
