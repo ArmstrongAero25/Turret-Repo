c31573f8fde59b6b06c37dc30ba16d76b8d39da1
HEXA-SOFTWARE-DEV <scottjohnjames0.8@gmail.com>
2024-06-07 19:26:52 +0100

    testing different sources

--- Turret/Turret_Code.py
+++ Turret/Turret_Code.py
@@ -20,7 +20,7 @@ def main():
     # cv2.startWindowThread() HEXA-SOFTWARE-DEV: Turns out
     # the Headless version of OpenCV python doesn't require this,
     # so I'm temporarily removing all things to do with window display.
-    cap = cv2.VideoCapture(0 + cv2.CAP_V4L2)
+    cap = cv2.VideoCapture(1 + cv2.CAP_V4L2)
 
     if not cap.isOpened():
         raise TE.videoException
